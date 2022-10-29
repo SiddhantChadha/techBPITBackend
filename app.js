2bb844fb2d11bfe7d9b1f72d9cf2272efc443c7c
SiddhantChadha <sid.01chadha@gmail.com>
2022-10-29 19:21:50 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('msg', (msg) => {
+        
+        console.log(msg);
+    })
+
+});
+
+server.listen(3000,()=>{
+    console.log("Server started");
+})
