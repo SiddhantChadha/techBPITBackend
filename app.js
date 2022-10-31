a555dba5223735fdc3b778f9d01a9a40af9beeae
SiddhantChadha <sid.01chadha@gmail.com>
2022-10-31 22:44:54 +0530

    fixes

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-
+app.get('/',(req,res)=>{
+    res.send("Dummy route");
+})
 
 io.on('connection', (socket) => {
     const user = socket.handshake.query.userId;
