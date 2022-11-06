86bffe4a3d4fd2074052d64b2708a6844903469d
SiddhantChadha <sid.01chadha@gmail.com>
2022-11-06 20:00:44 +0530

    bugs fixed

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,16 +5,27 @@ const sendMail = require('../services/mail');
 const signUp = async (req,res)=>{
     
     try{
-        const userExists = await User.findOne({email:req.body.email});
-        if(userExists){
+        const user = await User.findOne({email:req.body.email});
+        if(user && user.isActive){
             return res.status(400).send({message:"Email already in use"});
         }
+
         const generatedOTP = generateOTP();
 
+        if(user && !user.isActive){
+            await User.findByIdAndUpdate(user._id,{
+                $set:{otp:generatedOTP}
+            });
 
-        await User.create({email:req.body.email,password:req.body.password,otp:generatedOTP});
+        }else{
+            await User.create({email:req.body.email,password:req.body.password,otp:generatedOTP});
+        }
+
         const mailSent = await sendMail({to:req.body.email,otp:generatedOTP});
 
+        if(!mailSent){
+            return res.status(400).send({message:"Error in Signup. Try again"});
+        }
 
         return res.status(200).send({message:"Signup Successful"});
     }catch(err){
@@ -29,6 +40,10 @@ const login = async(req,res)=>{
         const user = await User.findOne({email:req.body.email});
         if(!user){
             return res.status(400).send({message:"Email not found"});
+        }
+
+        if(!user.isActive){
+            return res.status(400).send({message:"OTP not verified"});
         }
 
         if(user.password != req.body.password){
